a15d70b3fcc905fe16f1918e7fffdecae3069cf2
F Bliss <60014136+voffee@users.noreply.github.com>
2023-11-23 11:34:04 +0800

    JS Selectors & DOM Traversal

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 // DOM Manipulation
 
-/** @type {HTMLDivElement} */
+// /** @type {HTMLDivElement} */
 // const listItems = document.querySelectorAll('.list-items');
 
 // console.log(listItems);
@@ -13,12 +13,80 @@
 
 // Creating Elements
 
-/** @type {HTMLDivElement} */
-const ul = document.querySelector('ul');
+// /** @type {HTMLDivElement} */
+// const ul = document.querySelector('ul');
+
+// /** @type {HTMLDivElement} */
+// const licreation = document.createElement('li');
+
+// Adding elements
+
+// ul.append(licreation);
+
+// Modifying the text 
+
+// /** @type {HTMLDivElement} */
+// const firstListItem = document.querySelector('.list-items');
+
+// console.log(firstListItem.innerText);
+// console.log(firstListItem.textContent);
+
+// // Don't use innerHTML because it exposes to user how the program is structured.
+// console.log(firstListItem.innerHTML);
+
+// licreation.innerText = 'Ticket'
+
+// Modifying Attributes & Classes
+
+// licreation.setAttribute('class', 'main-heading');
+// licreation.removeAttribute('class');
+
+// const title = document.querySelector('.main-heading');
+
+// console.log(title.getAttribute('class'));
+
+// licreation.classList.add('example');
+// licreation.classList.remove('example');
+
+// Finding if element contains a certain class
+// console.log(licreation.classList.contains('list-items'));
+
+// Remove element
+
+// licreation.remove();
+
+// DOM TRAVERSAL
+
+// Parent Node Traversal
 
 /** @type {HTMLDivElement} */
-const licreation = document.createElement('li');
+let ul = document.querySelector('ul');
 
-ul.append(licreation);
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
 
-console.log();
+// const html = document.documentElement;
+
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+// Child Node Traversal
+
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+// ul.childNodes[1].style.backgroundColor = 'yellow';
+
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+// Sibling Node Traversal
+
+const div = document.querySelector('div');
+
+console.log(div.childNodes);
+
+console.log(ul.previousElementSibling);
+console.log(ul.nextSibling);
